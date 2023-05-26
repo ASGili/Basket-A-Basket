9561f89255c615ed98ff86b16a8e8d8519bb59c6
Aric Gilinsky <aric.gilinsky@gmail.com>
2023-05-26 15:55:12 +0100

    added total value of basket

--- src/components/UserBasket.js
+++ src/components/UserBasket.js
@@ -3,13 +3,17 @@ import BasketItem from "./BasketItem"
 const Basket = ({basket, handleDelFromBasket}) => {
 
     const userBasket = basket.map((item, index) => {return <BasketItem handleDelFromBasket={handleDelFromBasket} index={index} item={item} />
-
     })
 
+    const totalValue = basket.reduce((total, item)=>{return total + item.price},0)
+
     return(
+        <div>
+        <h5>The total value of your basket of baskets is ${totalValue}</h5>
         <ul>
         {userBasket}
         </ul>
+        </div>
     )
 }
 
